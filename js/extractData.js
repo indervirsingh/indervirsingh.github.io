85dc6e4fbafb1867db7c230e3d11bb309e4cc590
Indervir Singh <iindervirsingh@gmail.com>
2022-06-16 15:00:29 -0700

    small changes

--- js/extractData.js
+++ js/extractData.js
@@ -16,9 +16,8 @@ fs.readFile(url, 'utf-8', (err, data) => {
 
     // Extract JSON data to variables
     const { MATH: [ first, second, third ] } = jsonData
+    
 
-    // only first question from MATH is being extracted, need all math questions
-    console.log(second.choices.text_input)
 
 
 
