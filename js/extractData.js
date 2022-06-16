65b63e8ca55ed7202394427dcb73e2f777f8fef1
Indervir Singh <iindervirsingh@gmail.com>
2022-06-16 15:21:34 -0700

    updated extract method

--- js/extractData.js
+++ js/extractData.js
@@ -1,12 +1,9 @@
 const fs = require('fs')
 
-
+// URL path for JSON data
 const url = "../games/questions.json"
 
-
-
-
-
+// Read JSON file and extract data
 fs.readFile(url, 'utf-8', (err, data) => {
 
     if (err) throw err
@@ -15,9 +12,17 @@ fs.readFile(url, 'utf-8', (err, data) => {
     let jsonData = JSON.parse(data)
 
     // Extract JSON data to variables
-    const { MATH: [ first, second, third ] } = jsonData
-    
+    const {
+        MATH: [m1, m2, m3],
+        ENGLISH: [e1],
+        SCIENCE: [s1]
 
+    } = jsonData
+
+    // Store questions into separate arrays
+    const math_questions = [m1, m2, m3]
+    const english_questions = [e1]
+    const science_questions = [s1]
 
 
 
