a99f11b12428adbf0a346f7dc9f2d21295e0af5d
Indervir Singh <iindervirsingh@gmail.com>
2022-06-30 14:06:26 -0700

    server connected to db

--- server/db/index.js
+++ server/db/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 mongoose
-    .connect('mongodb://127.0.0.1:27017/questions', { useNewUrlParser: true })
+    .connect('mongodb://127.0.0.1:8080/questions', { useNewUrlParser: true })
     .catch(e => {
         console.log('Connection error', e.message)
     })
--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cors())
 app.use(bodyParser.json())
 
-db.on('error', console.error(console, 'MongoDB connection error:'))
+db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 
 app.get('/', (req, res) => {
     res.send('This is index.html')
