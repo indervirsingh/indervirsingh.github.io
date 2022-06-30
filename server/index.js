d17f6f2b2918321d445a3c857de6c5e88d9c3dd4
Indervir Singh <iindervirsingh@gmail.com>
2022-06-30 13:58:15 -0700

    added database and connected to server

--- server/index.js
+++ server/index.js
@@ -1,12 +1,17 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
+
+const db = require('./db')
+
 const app = express()
 const apiPort = 8080
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cors())
 app.use(bodyParser.json())
+
+db.on('error', console.error(console, 'MongoDB connection error:'))
 
 app.get('/', (req, res) => {
     res.send('This is index.html')
